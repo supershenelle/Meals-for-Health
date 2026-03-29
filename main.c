1fd7bc9583f70af2d91bd1092ab4032e3b4a7013
Shenelle Andrea G. Nono <shenellenono@gmail.com>
2026-03-29 05:43:44 +0000

    done

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
-// run code using: gcc main.c then call ./a.out in terminal
+/*********************************************************************************************************
+This is to certify that this project is my own work, based on my personal efforts in studying and applying 
+the concepts learned. I have constructed the functions and their respective algorithms and corresponding 
+code by myself. The program was run, tested, and debugged by my own efforts. I further certify that I have 
+not copied in part or whole or otherwise plagiarized the work of other students and/or persons, nor did I 
+employ the use of AI in any part of the deliverable.
 
-/*
-FINAL REVISIONS FOR EVERYONE:
-1. separate helper functions
-2. add comments to functions
-4. change password (bonus)
-5. add clear bg header
-6. test cases
-
-*/
+Nono, Shenelle Andrea - DLSU ID# 12505668
+Yatco, Marco Alfredo - DLSU ID# 
+*********************************************************************************************************/
 
 #include <stdio.h>
 #include "functions.h"
